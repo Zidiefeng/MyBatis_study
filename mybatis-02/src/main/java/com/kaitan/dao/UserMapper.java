e757bfe5425138c87490026ef5f509ba4986185a
Zidiefeng <sunkaitan123@163.com>
2021-07-04 13:41:53 -0400

    initial setup for mybatis02

---
+++ mybatis-02/src/main/java/com/kaitan/dao/UserMapper.java
@@ -0,0 +1,25 @@
+package com.kaitan.dao;
+
+import com.kaitan.pojo.User;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserMapper {
+
+    //查询全部用户
+    List<User> getUserList();
+
+    //根据id查询用户
+    User getUserById(int id);
+
+    //insert一个用户
+    int addUser(User user);
+
+    //update一个用户
+    int updateUser(User user);
+
+    //delete一个用户
+    int deleteUser(int id);
+
+}
---
+++ mybatis-02/src/test/java/dao/UserDaoTest.java
@@ -0,0 +1,22 @@
+package dao;
+
+import com.kaitan.dao.UserMapper;
+import com.kaitan.pojo.User;
+import com.kaitan.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+    @Test
+    public void getUserLike(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = mapper.getUserList();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+}

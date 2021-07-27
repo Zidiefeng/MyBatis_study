1e3ddce14c63c835d9b028fd23c4e355f65ed370
Zidiefeng <sunkaitan123@163.com>
2021-07-26 22:33:06 -0400

    logging

--- mybatis-04/src/test/java/com/kaitan/dao/UserDaoTest.java
+++ mybatis-04/src/test/java/com/kaitan/dao/UserDaoTest.java
@@ -3,9 +3,14 @@ package com.kaitan.dao;
 import com.kaitan.pojo .User;
 import com.kaitan.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
+
 public class UserDaoTest {
+
+    Logger logger = Logger.getLogger(UserDaoTest.class);
+
     @Test
     public void getUserList(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
@@ -13,5 +18,13 @@ public class UserDaoTest {
         User user = mapper.getUserById(1);
         System.out.println(user);
         sqlSession.close();
+    }
+
+    @Test
+    public void testLog4j(){
+        logger.info("info: 进入testLog4j");
+        logger.debug("debug: 进入testLog4j");
+        logger.error("error: 进入testLog4j");
+
     }
 }

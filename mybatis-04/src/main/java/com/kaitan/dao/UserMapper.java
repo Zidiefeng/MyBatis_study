3100740d08983f5cb20b013b067acda484ba0ca4
Zidiefeng <sunkaitan123@163.com>
2021-07-27 07:44:04 -0400

    RowBounds

--- mybatis-04/src/main/java/com/kaitan/dao/UserMapper.java
+++ mybatis-04/src/main/java/com/kaitan/dao/UserMapper.java
@@ -10,6 +10,9 @@ public interface UserMapper {
     //根据id查询用户
     User getUserById(int id);
 
-    //分页
+    //sql分页
     List<User> getUserByLimit(Map<String,Integer> map);
+
+    //用面向对象 分页
+    List<User> getUserByRowBounds();
 }
--- mybatis-04/src/test/java/com/kaitan/dao/UserDaoTest.java
+++ mybatis-04/src/test/java/com/kaitan/dao/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.kaitan.dao;
 
 import com.kaitan.pojo .User;
 import com.kaitan.utils.MybatisUtils;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -41,6 +42,23 @@ public class UserDaoTest {
         map.put("startIndex",0);
         map.put("pageSize",2);
         List<User> userList = mapper.getUserByLimit(map);
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void getUserByRowBounds(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        RowBounds rowBounds = new RowBounds(1, 2);
+
+
+        //通过Java代码实现分页，但是Mybatis官网都不推荐了
+        List<User> userList = sqlSession.selectList("com.kaitan.dao.UserMapper.getUserByRowBounds",
+                null,
+                rowBounds);
         for (User user : userList) {
             System.out.println(user);
         }

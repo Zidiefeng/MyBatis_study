b0b891aa23078b1bbd8808801c2fd2d587fed375
Zidiefeng <sunkaitan123@163.com>
2021-07-27 06:05:18 -0400

    limit

--- mybatis-04/src/main/java/com/kaitan/dao/UserMapper.java
+++ mybatis-04/src/main/java/com/kaitan/dao/UserMapper.java
@@ -2,9 +2,14 @@ package com.kaitan.dao;
 
 import com.kaitan.pojo.User;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserMapper {
 
     //根据id查询用户
     User getUserById(int id);
 
+    //分页
+    List<User> getUserByLimit(Map<String,Integer> map);
 }
--- mybatis-04/src/test/java/com/kaitan/dao/UserDaoTest.java
+++ mybatis-04/src/test/java/com/kaitan/dao/UserDaoTest.java
@@ -6,6 +6,10 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class UserDaoTest {
 
@@ -26,5 +30,20 @@ public class UserDaoTest {
         logger.debug("debug: 进入testLog4j");
         logger.error("error: 进入testLog4j");
 
+    }
+
+    @Test
+    public void getUserByLimit(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("startIndex",0);
+        map.put("pageSize",2);
+        List<User> userList = mapper.getUserByLimit(map);
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        sqlSession.close();
     }
 }
